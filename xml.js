47516d9b37e8165808b135299302590a5e9481a1
ms-shakil <mdshakil8796@gmail.com>
2024-02-14 19:00:09 +0600

    about xml

--- xml.js
+++ xml.js
@@ -1,16 +1,45 @@
+// event ----> onload() , onerror() // event works for something
+//property ---->response , responseText, responseType, responseUrl ,status, status text
+// function ---> open, send, setResquestHeader
+
 console.clear()
 
-const getData =( ) =>{
+const makeRequest=(method, url,data)=>{
     const xhr = new XMLHttpRequest()
-    xhr.open("GET","https://jsonplaceholder.typicode.com/posts")
+    xhr.open(method,url) // open function open api
+    
+    xhr.onload =()=>{ // onload evernt onload data
+       let data = xhr.response
+       console.log(JSON.parse(data)) // json.parse converst data json format to object formate
+    }
+    xhr.setRequestHeader("Content-Type","application/json") //json formate korte hobe
+   xhr.send(JSON.stringify(data)) // post request er smy json formate korte hobe....
 
-     xhr.onload =()=>{
-        let data = xhr.response
-        console.log(data)
-     }
-    xhr.send()
+
+   xhr.onerror =()=>{
+       console.log("have errror") // onerror  showing error 
+   }
+
+}
+
+const getData =( ) =>{
+    makeRequest("GET","https://jsonplaceholder.typicode.com/posts")
+  
+
 }
 
 
 
-getData()
+const sendData =( ) =>{
+    makeRequest("POST","https://jsonplaceholder.typicode.com/posts",{
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    })
+  
+
+}
+
+sendData()
+
+// getData()
